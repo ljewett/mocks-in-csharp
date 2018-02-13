066f6d52cdc1d731ecaab7eb9aaed157c90f4445
riojack <ayjay122@gmail.com>
2018-02-13 07:24:17 -0600

    Add more GWT

--- Claims/ClaimsService.cs
+++ Claims/ClaimsService.cs
@@ -3,6 +3,15 @@ namespace Claims {
     {
         public string HandleClaimSubmission(Claim claim)
         {
+            // Given I have a claim
+            // And the claim type is HAIL
+            // When I save the claim
+            // Then a single adjustment should also be created.
+
+            // Given I have a claim
+            // And the claim type is NOT HAIL
+            // When I save the claim
+            // Then no adjustment should be created.
             return null;
         }
     }
