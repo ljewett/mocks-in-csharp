774d8f3585c2aef44978fea2642cd714afd6ba09
riojack <ayjay122@gmail.com>
2018-02-12 21:01:29 -0600

    Add 3rd party persistence class lib project

---
+++ ThirdPartyPersistenceEngine/Persister.cs
@@ -0,0 +1,23 @@
+﻿namespace ThirdPartyPersistenceEngine
+{
+    /// <summary>
+    /// Provides basic Create, Read, Update, and Delete operations for a given type.
+    /// </summary>
+    public abstract class Persister<T>
+    {
+        /// <summary>
+        /// Inserts the object into the database or updates the existing record if record.Id already exists in the
+        /// underlying database.
+        /// </summary>
+        /// <param name="record">Record (object) to insert into the database.</param>
+        /// <returns>The ID created by the underlying database.</returns>
+        public abstract string Save(T record);
+
+        /// <summary>
+        /// Looks up an object in the database or returns null if it is not found.
+        /// </summary>
+        /// <param name="id">The ID of the object to look for.</param>
+        /// <returns></returns>
+        public abstract T Get(string id);
+    }
+}
