2bafadb339494fd13135d846cb12cdcac0b37876
Logan Jewett <ljewett@pillartechnology.com>
2018-02-21 14:06:32 -0600

    removing handle claim submission in service

--- Claims/ClaimService.cs
+++ Claims/ClaimService.cs
@@ -8,19 +8,5 @@ namespace Claims
         {
             _claimStorage = claimStorage;
         }
-
-        public string HandleClaimSubmission(Claim claim)
-        {
-            // Given I have a claim
-            // And the claim type is HAIL
-            // When I save the claim
-            // Then a single adjustment should also be created.
-
-            // Given I have a claim
-            // And the claim type is NOT HAIL
-            // When I save the claim
-            // Then no adjustment should be created.
-            return null;
-        }
     }
 }
