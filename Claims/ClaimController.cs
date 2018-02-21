0554f4240b11eb2f853780078fe61f93605d25f1
Logan Jewett <ljewett@pillartechnology.com>
2018-02-21 14:04:24 -0600

    removing get adjustments ticket in controller

--- Claims/ClaimController.cs
+++ Claims/ClaimController.cs
@@ -10,7 +10,7 @@ namespace Claims
         {
             _claimService = claimService;
         }
-
+        
         // POST /claims
         public string SaveClaim(Claim claim)
         {
@@ -18,17 +18,6 @@ namespace Claims
             // When I POST the claim to /claims
             // Then I should receive the claim ID back.
 
-            return null;
-        }
-        
-        // GET /claims/{claimId}/adjustments
-        public List<Adjustment> GetClaimAdjustments(string claimId)
-        {
-            // Given I have just saved a claim
-            // And I have the claim's ID
-            // When I do a GET on /claims/{claimId}/adjustments
-            // Then I should receive a list of adjustments for the claim.
-            
             return null;
         }
     }
